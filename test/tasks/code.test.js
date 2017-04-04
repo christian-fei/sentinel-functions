e83beb33d5c71a6497614bea9f737daabcb1f41b
Christian Fei <crifei93@gmail.com>
2017-04-04 23:25:42 +0200

    add first tests for bucket and code creation and deletion

--- test/tasks/code.test.js
+++ test/tasks/code.test.js
@@ -3,20 +3,30 @@ const config = testConfigFrom(require('../../config'))
 const delay = require('delay')
 const createBucket = require('../../lib/tasks/bucket/create')
 const deleteBucket = require('../../lib/tasks/bucket/delete')
-const {bucketExists} = require('../utils')
+const createCode = require('../../lib/tasks/code/create')
+const deleteCode = require('../../lib/tasks/code/delete')
+const {codeExists} = require('../utils')
 
-test('bucket', () => {
-  test.timeout('creates and deletes bucket', (done) => {
+test('code', () => {
+  test.timeout('creates and deletes code', (done) => {
     deleteBucket.run(config).then(() => {}).catch(() => {})
     .then(() => createBucket.run(config))
     .then(delay(1000))
-    .then(() => bucketExists(config))
-    .then(exists => ok(exists))
-    .then(() => deleteBucket.run(config))
+    .then(() => createCode.run(config))
     .then(delay(5000))
-    .then(() => bucketExists(config))
-    .then(exists => ok(!exists))
+    .then(() => codeExists(config))
+    .then(exists => ok(exists))
     .then(() => done())
+    .catch(done)
+  }, 30000)
+
+  test.timeout('deletes code', (done) => {
+    deleteCode.run(config)
+    .then(() => codeExists(config))
+    .then(delay(1000))
+    .then(exists => {
+      done(!exists ? null : new Error('code still exists!'))
+    })
     .catch(done)
   }, 30000)
 })
--- test/utils.js
+++ test/utils.js
@@ -1,15 +1,26 @@
 exports.bucketExists = bucketExists
+exports.codeExists = codeExists
 
 const AWS = require('aws-sdk')
 AWS.config.update({region: 'eu-west-1'})
 const {S3} = AWS
 const s3 = new S3({})
 
-function bucketExists (bucketName) {
+function bucketExists (options) {
   var params = {
-    Bucket: bucketName
+    Bucket: options.bucketName
   }
   return s3.headBucket(params).promise()
+  .then(() => true)
+  .catch(() => false)
+}
+
+function codeExists (options) {
+  var params = {
+    Bucket: options.bucketName,
+    Key: 'sentinel.zip'
+  }
+  return s3.headObject(params).promise()
   .then(() => true)
   .catch(() => false)
 }
