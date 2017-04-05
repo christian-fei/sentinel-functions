5f133e6727172db6e212489ddefa902fd2c30046
Christian Fei <crifei93@gmail.com>
2017-04-05 14:21:54 +0200

    refactoring: extract createCode, deleteCode and createCodeZip in Code module

--- test/tasks/code.test.js
+++ test/tasks/code.test.js
@@ -1,17 +1,16 @@
 const {ok} = require('assert')
 const Bucket = require('../../lib/bucket')
-const createCode = require('../../lib/tasks/code/create')
-const deleteCode = require('../../lib/tasks/code/delete')
+const Code = require('../../lib/code')
 const {codeExists, config} = require('../utils')
 
 test('code', () => {
   test.timeout('creates and deletes code', (done) => {
     Bucket.destroy(config).then(() => {}).catch(() => {})
     .then(() => Bucket.create(config))
-    .then(() => createCode.run(config))
+    .then(() => Code.create(config))
     .then(() => codeExists(config))
     .then(exists => ok(exists))
-    .then(() => deleteCode.run(config))
+    .then(() => Code.destroy(config))
     .then(() => codeExists(config))
     .then(exists => ok(!exists))
     .then(() => done())
